b5d0ec5251688c3775a749b94191437cd69de761
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-07-04 00:12:16 -0300

    bugfix: correcao no tratamento da mensagem ao remover uma operadora

--- src/app/core/ErroHandlerService.ts
+++ src/app/core/ErroHandlerService.ts
@@ -9,6 +9,7 @@ export class ErroHandlerService {
     constructor(private notificacao: NotificacaoService) { }
 
     capturar(errorResponse: any) {
+        console.log(errorResponse.error)
         for (const erro of errorResponse.error) {
             this.notificacao.error(erro.mensagem);
         }
