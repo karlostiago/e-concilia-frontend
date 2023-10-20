a75d548cc195041d993233940c4c5caac14d415c
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-10-20 14:35:45 -0300

    feat: inclusao do grafico de vendas

--- src/app/features/dashboard/dashboard-inicial/dashboard-inicial.component.ts
+++ src/app/features/dashboard/dashboard-inicial/dashboard-inicial.component.ts
@@ -20,11 +20,63 @@ export class DashboardInicialComponent implements OnInit {
     empresas = new Array<Empresa>();
     empresaSelecionadaId = -1;
 
+    basicData: any;
+    basicOptions: any;
+
     constructor(private dashboardService: DashboardService,
                 private empresaService: EmpresaService,) {
     }
 
     ngOnInit(): void {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+        this.basicData = {
+            labels: ['12/10', '13/10', '14/10', '15/10', '16/10', '17/10', '18/10'],
+            datasets: [
+                {
+                    label: 'Vendas',
+                    data: [540.20, 325.99, 702.58, 620.32, 835.25, 1100.99, 225.18],
+                    backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+                    borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+                    borderWidth: 1,
+                }
+            ]
+        };
+
+        this.basicOptions = {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder,
+                        drawBorder: false
+                    }
+                }
+            }
+        };
+
         this.pesquisar();
         this.carregarEmpresas();
     }
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DashboardRoutingModule} from "./dashboard-routing.module";
 import { DashboardInfoComponent } from './dashboard-card/dashboard-info.component';
+import {ChartModule} from "primeng/chart";
+
+import * as ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @NgModule({
     declarations: [
         DashboardInfoComponent
     ],
     exports: [
-        DashboardInfoComponent
+        DashboardInfoComponent,
+        ChartModule
     ],
     imports: [
         CommonModule,
-        DashboardRoutingModule
+        DashboardRoutingModule,
+        ChartModule
     ]
 })
 export class DashboardModule { }
