3387ec00e1714eb61870f949b5ea86f33e2414a3
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-01-25 23:56:34 -0300

    feat: implemetacao da funcionalidade de cadastro e atualizacao, melhoria no bloqueio dos checkbox nao permitidos.

---
+++ src/app/features/configuracao/seguranca/regra.service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {ErroHandlerService} from "../../../core/ErroHandlerService";
+import {AbstractService} from "../../../service/AbstractService";
+import {Usuario} from "../../../model/Usuario";
+import {FiltroUsuario} from "../../../filter/FiltroUsuario";
+import {SegurancaService} from "../../seguranca/seguranca.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService extends AbstractService<Usuario> {
+
+    constructor(private httpClient: HttpClient,
+                protected override error: ErroHandlerService) {
+        super(error);
+    }
+
+    pathURL() {
+        return 'usuarios';
+    }
+
+    async salvar (usuario: Usuario): Promise<Usuario> {
+        const request = this.httpClient.post(`${this.baseURL}/${this.pathURL()}`, JSON.stringify(usuario), this.options());
+        return this.toPromise(request);
+    }
+
+    async pesquisarPorId (id: number): Promise<Usuario> {
+        const request =this.httpClient.get(`${this.baseURL}/${this.pathURL()}/${id}`, this.options());
+        return this.toPromise(request);
+    }
+
+    async editar (usuario: Usuario): Promise<Usuario> {
+        const request = this.httpClient.put(`${this.baseURL}/${this.pathURL()}/${usuario.id}`, JSON.stringify(usuario), this.options());
+        return this.toPromise(request);
+    }
+
+    async pesquisar (filtroUsuario: FiltroUsuario): Promise<Usuario[]> {
+        const request = this.httpClient.get(`${this.baseURL}/${this.pathURL()}?nomeCompleto=${filtroUsuario.nomeCompleto}&email=${filtroUsuario.email}`, this.options());
+        return this.toPromise(request);
+    }
+
+    async excluir (id: number) {
+        const request = this.httpClient.delete(`${this.baseURL}/${this.pathURL()}/${id}`, this.options());
+        return this.toPromise(request);
+    }
+}
