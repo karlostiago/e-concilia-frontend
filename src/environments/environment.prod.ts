619904648fd780f64068aa312771301f6e51f2f4
Carlos Tiago <carlostiagodesousa@gmail.com>
2024-04-30 19:42:29 -0300

    feat: atualizando versao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://e-concilia-api.herokuapp.com/api',
-    version: '0.3.1-Snapshot',
-    dataAtualizacao: '27/03/2024'
+    version: '0.3.4-Snapshot',
+    dataAtualizacao: '01/05/2024'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8081/econcilia/api',
-    version: '0.3.1-Snapshot',
-    dataAtualizacao: '27/03/2024'
+    version: '0.3.4-Snapshot',
+    dataAtualizacao: '01/05/2024'
 }
